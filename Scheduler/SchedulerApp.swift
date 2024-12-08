4980f10efbb4ad6ef3d40323ae490a276e5a3ead
Vikas <vikas@higgsbosonhealth.com>
2024-12-08 12:14:54 +0530

    Initial Commit

---
+++ Scheduler/SchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchedulerApp.swift
+//  Scheduler
+//
+//  Created by Vikas Joshi on 08/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
